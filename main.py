5a7cd6b549c92765c959a7b79b436bea6f382aaf
michaelwahlin <michael@wahlin.net>
2024-12-27 08:47:08 -0500

    Add asteroid and asteroidfield

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     print("Starting asteroids!")
@@ -18,10 +20,14 @@ def main():
 
     updatables = pygame.sprite.Group()
     drawables = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
 
     Player.containers = (updatables, drawables)
+    Asteroid.containers = (asteroids, updatables, drawables)
+    AsteroidField.containers = (updatables)
 
     player = Player(x, y)
+    asteroid_field = AsteroidField()
 
     while True:
 
