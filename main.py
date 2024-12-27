8c1cd7025ab5b834a3677b14a0e9738708a24905
michaelwahlin <michael@wahlin.net>
2024-12-27 10:44:39 -0500

    Add logic to split asteroid

--- main.py
+++ main.py
@@ -49,7 +49,7 @@ def main():
 
             for shot in shots:
                 if asteroid.collision(shot):
-                    asteroid.kill()
+                    asteroid.split()
                     shot.kill()
                 
         screen.fill("black")
